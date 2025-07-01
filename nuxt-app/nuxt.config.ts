aa5e388ae179da5801b158583f0e19a2ea0d3c0d
Никита Остапенко <ostapenko.nikits@yandex.ru>
2025-07-02 04:24:01 +1000

    last task + fixes

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,21 +1,31 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// https://nuxt.com/docs/api/configuration/nuxt-config 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+
   modules: [
-    ["@nuxtjs/google-fonts", {
-      families: {
-        'Open Sans': [400, 700],
-        'Montserrat': true,
-      }
-    }],
-    "nuxt-swiper",
+    [
+      '@nuxtjs/google-fonts',
+      {
+        families: {
+          'Open Sans': [400, 700],
+          Montserrat: true,
+        },
+      },
+    ],
+    'nuxt-swiper',
     'reka-ui/nuxt',
-    'vue-yandex-maps/nuxt',
+    // 'vue-yandex-maps/nuxt',
   ],
-  css: ['~/assets/css/dialog.css', '~/assets/css/main.css', , '~/assets/css/pagination.css'],
+
+  css: [
+    '~/assets/css/dialog.css',
+    '~/assets/css/main.css',
+    '~/assets/css/pagination.css',
+  ],
+
   yandexMaps: {
-    apikey: 'cf4b9da0-b9b5-4e86-b783-ab47290b9a86',
-    lang: 'ru_RU'
-  }
+    apikey: process.env.YANDEX_MAPS_API_KEY,
+    version: '2.1',
+  },
 })
